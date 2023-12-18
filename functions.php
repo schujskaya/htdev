47e7c20f6198ee9349d7ef3d2770e49bb98c35df
schujskaya <schujskaya@mail.ru>
2023-12-19 00:20:13 +0400

    add functions.php

---
+++ functions.php
@@ -0,0 +1,166 @@
+<?php
+
+register_nav_menus(
+	array(
+		'head_menu' => 'Шапка сайта'
+	)
+);
+
+class My_Walker_Nav_Menu extends Walker_Nav_Menu {
+
+	// add classes to ul sub-menus
+	function start_lvl( &$output, $depth = 0, $args = NULL ) {
+		// depth dependent classes
+		$indent = ( $depth > 0  ? str_repeat( "\t", $depth ) : '' ); // code indent
+		$display_depth = ( $depth + 1); // because it counts the first submenu as 0
+		$classes = array(
+			'sub-menu',
+			( $display_depth % 2  ? 'menu-odd' : 'menu-even' ),
+			( $display_depth >=2 ? 'sub-sub-menu' : '' ),
+			'menu-depth-' . $display_depth
+		);
+		$class_names = implode( ' ', $classes );
+
+		// build html
+		$output .= "\n" . $indent . '<div class="' . $class_names . '">' . "\n";
+	}
+
+	// add main/sub classes to li's and links
+	function start_el( &$output, $data_object, $depth = 0, $args = null, $current_object_id = 0 ) {
+		global $wp_query;
+
+		// Restores the more descriptive, specific name for use within this method.
+		$item = $data_object;
+
+		$indent = ( $depth > 0 ? str_repeat( "\t", $depth ) : '' ); // code indent
+
+		// depth dependent classes
+		$depth_classes = array(
+			( $depth == 0 ? 'main-menu-item' : 'sub-menu-item' ),
+			( $depth >=2 ? 'sub-sub-menu-item' : '' ),
+			( $depth % 2 ? 'menu-item-odd' : 'menu-item-even' ),
+			'menu-item-depth-' . $depth
+		);
+		$depth_class_names = esc_attr( implode( ' ', $depth_classes ) );
+
+		// passed classes
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		$class_names = esc_attr( implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) ) );
+
+		// build html
+		$output .= $indent . '<div id="nav-menu-item-'. $item->ID . '" class="' . $depth_class_names . ' ' . $class_names . '">';
+
+		// link attributes
+		$attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+		$attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+		$attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+		$attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+		$attributes .= ' class="menu-link ' . ( $depth > 0 ? 'sub-menu-link' : 'main-menu-link' ) . '"';
+
+		$item_output = sprintf( '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
+			$args->before,
+			$attributes,
+			$args->link_before,
+			apply_filters( 'the_title', $item->title, $item->ID ),
+			$args->link_after,
+			( $depth > 0 ? '</div>' : ' ' )
+		);
+
+		// build html
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+	
+	function end_el( &$output, $data_object, $depth = 0, $args = array() ) {
+		$output .= "</div>\n";
+	}
+
+}
+
+
+add_action( 'init', 'create_taxonomy' );
+function create_taxonomy(){
+
+	// список параметров: wp-kama.ru/function/get_taxonomy_labels
+	register_taxonomy( 'taxonomy', [ 'post_type_test' ], [
+		'label'                 => '', 
+		'labels'                => [
+			'name'              => 'Рубрики тестовой записи',
+			'singular_name'     => 'Рубрика тестовой записи',
+			'search_items'      => 'Искать рубрику тестовой записи',
+			'all_items'         => 'Все рубрики тестовой записи',
+			'view_item '        => 'Смотреть рубрику тестовой записи',
+			'parent_item'       => 'Родительская рубрика',
+			'parent_item_colon' => 'Родительская рубрика:',
+			'edit_item'         => 'Изменить рубрику тестовой записи',
+			'update_item'       => 'Обновить рубрику тестовой записи',
+			'add_new_item'      => 'Добавить новую рубрику тестовой записи',
+			'new_item_name'     => 'Название рубрики тестовой записи',
+			'menu_name'         => 'Рубрики тестовой записи',
+			'back_to_items'     => '← Назад к рубрикам тестовой записи',
+		],
+		'description'           => '', // описание таксономии
+		'public'                => true,
+		// 'publicly_queryable'    => null, // равен аргументу public
+		// 'show_in_nav_menus'     => true, // равен аргументу public
+		// 'show_ui'               => true, // равен аргументу public
+		// 'show_in_menu'          => true, // равен аргументу show_ui
+		// 'show_tagcloud'         => true, // равен аргументу show_ui
+		// 'show_in_quick_edit'    => null, // равен аргументу show_ui
+		'hierarchical'          => true,
+
+		'rewrite'               => true,
+		//'query_var'             => $taxonomy, // название параметра запроса
+		'capabilities'          => array(),
+		'meta_box_cb'           => null, // html метабокса. callback: `post_categories_meta_box` или `post_tags_meta_box`. false — метабокс отключен.
+		'show_admin_column'     => false, // авто-создание колонки таксы в таблице ассоциированного типа записи. (с версии 3.5)
+		'show_in_rest'          => null, // добавить в REST API
+		'rest_base'             => null, // $taxonomy
+		// '_builtin'              => false,
+		//'update_count_callback' => '_update_post_term_count',
+	] );
+}
+
+add_action( 'init', 'register_post_types' );
+
+function register_post_types(){
+
+	register_post_type( 'post_type_test', [
+		'label'  => null,
+		'labels' => [
+			'name'               => 'Тестовый тип записи', // основное название для типа записи
+			'singular_name'      => 'Тестовая запись', // название для одной записи этого типа
+			'add_new'            => 'Добавить новую тестовую запись', // для добавления новой записи
+			'add_new_item'       => 'Добавление новой тестовой записи', // заголовка у вновь создаваемой записи в админ-панели.
+			'edit_item'          => 'Редактирование тестовой записи', // для редактирования типа записи
+			'new_item'           => 'Новая тестовая запись', // текст новой записи
+			'view_item'          => 'Смотреть тестовую запись', // для просмотра записи этого типа.
+			'search_items'       => 'Искать тестовую запись', // для поиска по этим типам записи
+			'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
+			'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+			'parent_item_colon'  => '', // для родителей (у древовидных типов)
+			'menu_name'          => 'Тестовый тип', // название меню
+		],
+		'description'            => '',
+		'public'                 => true,
+		// 'publicly_queryable'  => null, // зависит от public
+		// 'exclude_from_search' => null, // зависит от public
+		// 'show_ui'             => null, // зависит от public
+		// 'show_in_nav_menus'   => null, // зависит от public
+		'show_in_menu'           => null, // показывать ли в меню админки
+		// 'show_in_admin_bar'   => null, // зависит от show_in_menu
+		'show_in_rest'        => null, // добавить в REST API. C WP 4.7
+		'rest_base'           => null, // $post_type. C WP 4.7
+		'menu_position'       => null,
+		'menu_icon'           => null,
+		//'capability_type'   => 'post',
+		//'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+		//'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+		'hierarchical'        => false,
+		'supports'            => [ 'title', 'editor' ], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+		'taxonomies'          => [],
+		'has_archive'         => false,
+		'rewrite'             => true,
+		'query_var'           => true,
+	] );
+
+}
